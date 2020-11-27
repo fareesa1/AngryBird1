3f91a67fff63513988537d131eb9f950681ebefd
fareesa_mas <30589399+fareesa1@users.noreply.github.com>
2020-11-27 19:58:08 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -104,7 +104,7 @@ function keyPressed(){
 }
 
 async function getBackgroundImg(){
-    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/japan");
     var responseJSON = await response.json();
 
     var datetime = responseJSON.datetime;
