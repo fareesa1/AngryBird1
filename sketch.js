a06e8e35286095ea20f1911e6f60a528dc23778f
fareesa1 <fareesamasroor@gmail.com>
2021-05-21 20:15:19 +0530

    change structure

--- sketch.js
+++ sketch.js
@@ -32,15 +32,7 @@ function setup(){
     
     log1 = new Log(810,260,300, PI/2);
 
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
-
-    log3 =  new Log(810,180,300, PI/2);
-
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
+   
 
     bird = new Bird(200,50);
     bird1 = new Bird(150,150);
@@ -67,16 +59,8 @@ function draw(){
     pig1.score();
     log1.display();
 
-    box3.display();
-    box4.display();
-    pig3.display();
-    pig3.score();
-    log3.display();
-
-    box5.display();
-    log4.display();
-    log5.display();
-
+   
+    
     bird.display();
     bird1.display()
     bird2.display()
