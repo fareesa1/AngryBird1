12bcda77ec0ee940c5db92c82754a3ace692a7ca
CY252186 <chinmaynagasai.yalamati.186@k12.friscoisd.org>
2021-05-31 11:53:19 -0500

    pig added

--- Pig.js
+++ Pig.js
@@ -13,7 +13,6 @@ class Pig extends BaseClass {
    else{
      World.remove(world, this.body);
      push();
-     this.Visiblity = this.Visiblity - 5;
      tint(255,this.Visiblity);
      image(this.image, this.body.position.x, this.body.position.y, 50, 50);
      pop();
